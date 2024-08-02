09ccaff19b1d7a57758f783ef08b5829607bc7a3
Eljones <eljones.odongo@gmail.com>
2024-08-02 18:40:45 +0300

    Add locale selection with request.accept_languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+""" determining requested language best match with supported languages """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Config class for babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get the best match between the request's accepted languages and the
+    supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    ''' landing page url '''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
